6071404d75dc705a0ca5e7458f0aeee1e1480f3a
angeloCC <56007679+angelochaves@users.noreply.github.com>
2020-06-29 18:54:41 -0300

    commit 1

--- screensaver.py
+++ screensaver.py
@@ -1,3 +1,37 @@
-import tkinter
+import sys
 import pygame
+from pygame.locals import *
 
+pygame.init()
+
+clock = pygame.time.Clock()
+figura = pygame.image.load("./figura.jpg")
+janela = pygame.display.set_mode([1366, 768])
+
+rodando = True
+
+x = 357
+y = 54
+sx = 130
+sy = 50
+
+while rodando:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            rodando = False
+
+    delta = clock.tick(30) / 1000
+    x += sx * delta
+    y += sy * delta
+
+    if x + 204 > janela.get_width() or x < 0:
+        sx *= -1
+
+    if y + 192 > janela.get_height() or y < 0:
+        sy *= -1
+
+    janela.fill((0,0,0))
+    janela.blit(figura, (x,y))
+    pygame.display.update()
+
+pygame.quit()
