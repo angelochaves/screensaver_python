88cadf148f5000e807c3186d09e2592e033277f7
angeloCC <56007679+angelochaves@users.noreply.github.com>
2020-06-30 16:49:34 -0300

    commit 4

--- screensaver2.py
+++ screensaver2.py
@@ -5,15 +5,9 @@ from pygame.locals import *
 pygame.init()
 
 clock = pygame.time.Clock()
-figura = pygame.image.load("./figura.jpg")
 janela = pygame.display.set_mode([1366, 768], pygame.FULLSCREEN)
 
 rodando = True
-
-x = 357
-y = 54
-sx = 130
-sy = 50
 
 while rodando:
     for event in pygame.event.get():
@@ -23,17 +17,8 @@ while rodando:
             rodando = False
 
     delta = clock.tick(30) / 1000
-    x += sx * delta
-    y += sy * delta
-
-    if x + 204 > janela.get_width() or x < 0:
-        sx *= -1
-
-    if y + 192 > janela.get_height() or y < 0:
-        sy *= -1
 
     janela.fill((0,0,0))
-    janela.blit(figura, (round(x),round(y)))
     pygame.display.update()
 
 pygame.quit()
